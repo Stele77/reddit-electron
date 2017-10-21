0b2349c0afaf2c84bf1c656f1da203c8fc2e599a
AustinKeen <akeen@iastate.edu>
2017-10-20 21:55:12 -0500

    shit

---
+++ src/components/secondaryMenu.tsx
@@ -0,0 +1,10 @@
+import * as React from 'React';
+
+export class SecondaryMenu extends React.Component {
+    
+    render() {
+        return (
+            <div className="secondMenu"></div>
+        )
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import * as ReactDOM from "react-dom";
 
 import { Header } from './components/header';
 import { Sidebar } from './components/sidebar';
+import { SecondaryMenu } from './components/secondaryMenu';
 import './index.css';
 
 ReactDOM.render(
     <div>
         <Header />
         <Sidebar />
+        <SecondaryMenu />
     </div>
 
     ,
