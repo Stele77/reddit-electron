bef54019d110f553ec5f9094db2f56c4556ea73d
Ross Reicks <ross@reicks-mbp.student.iastate.edu>
2017-10-21 21:44:27 -0500

    again...

--- src/containers/app.tsx
+++ src/containers/app.tsx
@@ -18,7 +18,7 @@ export class App extends React.Component<any> {
 
     constructor(props: any) {
         super(props);
-        axios.defaults.headers.common['Authorization'] = "Bearer 5x92Zj_PS29MUp4j85YmAGEBVW8";
+        axios.defaults.headers.common['Authorization'] = "Bearer oiE8Ogt4-6R_P715dyiV4cayNyg";
         axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
     }
 
